98ddf6c3a46c426f4119e509d72f14a2553e4410
Yuta Masuda <massu2000.1019@gmail.com>
2021-07-16 20:09:30 +0900

    ⚡️ 名言の非同期処理のエラーやローディング時の処理

--- src/Components/Quote/index.jsx
+++ src/Components/Quote/index.jsx
@@ -3,22 +3,45 @@ import { useCallback, useEffect, useState } from "react";
 
 export const Quote = () => {
   const [quote, setQuote] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   const getFamousQuote = useCallback(async () => {
-    const res = await fetch("https://animechan.vercel.app/api/random");
-    const json = await res.json();
-    console.log(json);
-    setQuote(json);
+    try {
+      const res = await fetch("https://animechan.vercel.app/api/random");
+      if (!res.ok) {
+        throw new Error("エラーが発生したため、データの取得に失敗しました");
+      }
+      const json = await res.json();
+      setQuote(json);
+    } catch (error) {
+      setError(error);
+    }
+    setLoading(false);
   }, []);
 
   useEffect(() => {
     getFamousQuote();
   }, []);
 
+  if (loading) {
+    return <h1>4animater....</h1>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (quote.anime === undefined) {
+    return <div>No data</div>;
+  }
+
   return (
     <div className={styles.quote}>
       <h1>{quote.quote}</h1>
       <p className={styles.quoteInfo}>
-        {"chara : " + quote.character + ","} <span>{" "}</span> {"title : " + quote.anime}
+        {"chara : " + quote.character + ","} <span> </span>{" "}
+        {"title : " + quote.anime}
       </p>
     </div>
   );
