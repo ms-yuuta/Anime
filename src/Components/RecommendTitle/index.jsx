8b9d811316aa2be528d28f9657f780a837aeb8a5
Yuta Masuda <massu2000.1019@gmail.com>
2021-07-25 11:35:58 +0900

    💄 スタイルの微調整

--- src/Components/RecommendTitle/index.jsx
+++ src/Components/RecommendTitle/index.jsx
@@ -2,10 +2,9 @@ import styles from "src/Components/RecommendTitle/RecommendTitle.module.css";
 
 export const RecommendTitle = () => {
   return (
-    <div className={styles.header}>
+    <div className={styles.titleContent}>
       <h2 className={styles.title}>”10秒”で終わらせる適正アニメ</h2>
       <p className={styles.title}>〜独断と偏見なのは許してください〜 </p>
-      <p>（ブサイクなデザインですいません）</p>
     </div>
   );
 };
