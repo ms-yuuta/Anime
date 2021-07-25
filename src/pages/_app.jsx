74cd2fae1ec8370a275364cf16c2c47b4791f843
Yuta Masuda <massu2000.1019@gmail.com>
2021-07-25 11:34:48 +0900

    ♻️ Headコンポーネントの共通化

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,12 @@
 import styles from 'src/styles/globals.css'
 import { Headline } from 'src/Components/Common/Headline'
 import { Footer } from 'src/Components/Common/Footer'
+import { Header } from 'src/Components/Common/Header'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Header/>
       <Headline />
       <Component {...pageProps} />
       <p>
