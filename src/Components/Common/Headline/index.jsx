dda8c35766d63f964fef1d900861f9d99f96c460
Yuta Masuda <massu2000.1019@gmail.com>
2021-11-03 15:14:23 +0900

    ✨ AniMapの追加

--- src/Components/Common/Headline/index.jsx
+++ src/Components/Common/Headline/index.jsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 export const Headline = () => {
   const LINKS = [
-    { href: "/recommend", class: "", title: "Recommend" },
-    { href: "/work", class: styles.api, title: "Works" },
+    { href: "/recommend", class: "", title: "character" },
+    // { href: "/work", class: styles.api, title: "Works" },
   ];
 
   return (
@@ -20,6 +20,7 @@ export const Headline = () => {
         <span className={styles.nextVersion}>beta</span>
       </p>
       <div className={styles.links}>
+        <a href="https://animap.vercel.app/">AniMap</a>
         {LINKS.map((link) => {
           return (
             <Link href={link.href} key={link.href}>
